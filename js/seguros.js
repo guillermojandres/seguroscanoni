0e1f7e43cb1f8006fd048cd72ae655230c1787eb
Guillermo Rafael Vasquez Castaneda <memuxit@gmail.com>
2020-05-28 00:00:09 -0600

    ➕ feat: Add glossary page and styles

--- js/seguros.js
+++ js/seguros.js
@@ -6,4 +6,18 @@ $(document).ready(function () {
             $("header").removeClass("header2");
         }
     });
+
+    $('.card').click(function () {
+        if ($(this).find('.collapse').hasClass('show')) {
+            $(this).find('.card-header').removeClass('collapse-title-show');
+            $(this).find('i').removeClass('fa fa-minus-circle active').addClass('fa fa-plus-circle');
+            $(this).find('span').addClass('collapse-title');
+            $(this).find('button').css('color', 'black');
+        } else {
+            $(this).find('.card-header').addClass('collapse-title-show');
+            $(this).find('i').removeClass('fa fa-plus-circle').addClass('fa fa-minus-circle active');
+            $(this).find('span').removeClass('collapse-title');
+            $(this).find('button').css('color', 'white');
+        }
+    });
 });
