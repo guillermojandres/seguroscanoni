0fafcb4ef01becc42cd9cf3e2545e5ceb23e4b83
# <exit>
2020-05-29 15:00:15 -0600

    Menu-responsive

--- js/seguros.js
+++ js/seguros.js
@@ -6,6 +6,29 @@ $(document).ready(function () {
             $("header").removeClass("header2");
         }
     });
+
+    $('.navbar-toggler').on('click', function(){
+		openNav();
+	});
+
+	$('.mobile-overwall').on('click', function(){
+		closeNav();
+	});
    
 });
 
+function openNav() {
+	
+	var height = $(window).height();
+	$(".navbar-collapse").css({'display': 'inline'});
+	$(".navbar-collapse").css({'width': '190px'});
+	$('.navbar-collapse').height(height);
+	$('.mobile-overwall').css({'visibility' : 'visible'});
+
+}
+
+function closeNav() {
+	$(".navbar-collapse").css({'width': '0px', 'transition': '0.5s'});
+	$('.navbar-collapse').height(0);
+	$(".mobile-overwall").css({'visibility': 'hidden'});
+}
