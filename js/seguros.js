03d93f15fc94925371b7d35e3a2ccae434466b38
Guillermo Rafael Vasquez Castaneda <memuxit@gmail.com>
2020-06-06 13:26:07 -0600

    ➕ feat: Complete styles for allies

--- js/seguros.js
+++ js/seguros.js
@@ -1,14 +1,27 @@
 $(document).ready(function () {
 
-	$('#TableMedico').DataTable({
+	$('.table-allies').DataTable({
+		"paging": false,
+		"info": false,
 		"language": {
-			"sProcessing":    "Procesando...",
-			"sSearch":        "Buscar:",
+			"search": "_INPUT_",
+			"searchPlaceholder": "Buscar...",
+			"sProcessing":     "Procesando...",
+			"sLengthMenu":     "Mostrar _MENU_ registros",
+			"sZeroRecords":    "No se encontraron resultados",
+			"sEmptyTable":     "Ningún dato disponible en esta tabla",
+			"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+			"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+			"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+			"sInfoPostFix":    "",
+			"sSearch":         "Buscar:",
+			"sUrl":            "",
+			"sInfoThousands":  ",",
 			"sLoadingRecords": "Cargando...",
 			"oPaginate": {
 				"sFirst":    "Primero",
-				"sLast":    "Último",
-				"sNext":    "Siguiente",
+				"sLast":     "Último",
+				"sNext":     "Siguiente",
 				"sPrevious": "Anterior"
 			},
 			"oAria": {
