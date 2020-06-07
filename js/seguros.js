4c38f25dd6f2dfebd05b5ae1cf45ac89df529f90
Guillermo Rafael Vasquez Castaneda <memuxit@gmail.com>
2020-06-06 20:51:03 -0600

    💇 style: Add styles for cards within the story

--- js/seguros.js
+++ js/seguros.js
@@ -1,35 +1,35 @@
 $(document).ready(function () {
 
-	$('.table-allies').DataTable({
-		"paging": false,
-		"info": false,
-		"language": {
-			"search": "_INPUT_",
-			"searchPlaceholder": "Buscar...",
-			"sProcessing":     "Procesando...",
-			"sLengthMenu":     "Mostrar _MENU_ registros",
-			"sZeroRecords":    "No se encontraron resultados",
-			"sEmptyTable":     "Ningún dato disponible en esta tabla",
-			"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-			"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-			"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-			"sInfoPostFix":    "",
-			"sSearch":         "Buscar:",
-			"sUrl":            "",
-			"sInfoThousands":  ",",
-			"sLoadingRecords": "Cargando...",
-			"oPaginate": {
-				"sFirst":    "Primero",
-				"sLast":     "Último",
-				"sNext":     "Siguiente",
-				"sPrevious": "Anterior"
-			},
-			"oAria": {
-				"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-				"sSortDescending": ": Activar para ordenar la columna de manera descendente"
-			}
-		}
-	 });
+    $('.table-allies').DataTable({
+        "paging": false,
+        "info": false,
+        "language": {
+            "search": "_INPUT_",
+            "searchPlaceholder": "Buscar...",
+            "sProcessing": "Procesando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "No se encontraron resultados",
+            "sEmptyTable": "Ningún dato disponible en esta tabla",
+            "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar:",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        }
+    });
 
 
     $(window).scroll(function () {
@@ -40,49 +40,49 @@ $(document).ready(function () {
         }
     });
 
-    $('.navbar-toggler').on('click', function(){
-		openNav();
-	});
+    $('.navbar-toggler').on('click', function () {
+        openNav();
+    });
 
-	$('.mobile-overwall').on('click', function(){
-		closeNav();
-	});
+    $('.mobile-overwall').on('click', function () {
+        closeNav();
+    });
 
-	$(".btn-link").click(function(){
+    $(".btn-link").click(function () {
 
-	 $("#accordionExample").toggleClass("main");	
-	 });
-	 
-	 $("#btn-leer").click(function(){
-		
-		$("#more-text").addClass('esvisible');
-		$("#more-text").removeClass('es0culto');
-		$('#btn-leer').css('display','none');
-		$("#parrafo").css('display','block');
-	 });
+        $("#accordionExample").toggleClass("main");
+    });
 
-	 $('#MyModal').click(function(){
-		$("#Oficinas").modal({backdrop: true, keyboard: false});
-	   });
-	   
-	   $('#MyModal2').click(function(){
-		 $('#Oficinas').modal('hide');
-			$("#Oficinas2").modal({backdrop: true, keyboard: false});
-	   });
+    $("#btn-leer").click(function () {
+
+        $("#more-text").addClass('esvisible');
+        $("#more-text").removeClass('es0culto');
+        $('#btn-leer').css('display', 'none');
+        $("#parrafo").css('display', 'block');
+    });
+
+    $('#MyModal').click(function () {
+        $("#Oficinas").modal({backdrop: true, keyboard: false});
+    });
+
+    $('#MyModal2').click(function () {
+        $('#Oficinas').modal('hide');
+        $("#Oficinas2").modal({backdrop: true, keyboard: false});
+    });
 });
 
 function openNav() {
-	
-	var height = $(window).height();
-	$(".navbar-collapse").css({'display': 'inline'});
-	$(".navbar-collapse").css({'width': '190px'});
-	$('.navbar-collapse').height(height);
-	$('.mobile-overwall').css({'visibility' : 'visible'});
+
+    var height = $(window).height();
+    $(".navbar-collapse").css({'display': 'inline'});
+    $(".navbar-collapse").css({'width': '190px'});
+    $('.navbar-collapse').height(height);
+    $('.mobile-overwall').css({'visibility': 'visible'});
 
 }
 
 function closeNav() {
-	$(".navbar-collapse").css({'width': '0px', 'transition': '0.5s'});
-	$('.navbar-collapse').height(0);
-	$(".mobile-overwall").css({'visibility': 'hidden'});
+    $(".navbar-collapse").css({'width': '0px', 'transition': '0.5s'});
+    $('.navbar-collapse').height(0);
+    $(".mobile-overwall").css({'visibility': 'hidden'});
 }
